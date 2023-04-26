9f0ca951e8f5b5c5ede8b8a2b27532e3f46930bf
gabriel-victor933 <gabrielvictor933@usp.br>
2023-04-26 19:26:11 -0300

    Creating application routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
   return( 
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>main</div>} />
+            <Route path="/cart" element={<></>} />
+            <Route path="/signIn" element={<></>} />
+            <Route path="/signUp" element={<></>} />
+            <Route path="/checkout" element={<></>} />
+          </Routes>
+    </BrowserRouter>
   );
 }
 
