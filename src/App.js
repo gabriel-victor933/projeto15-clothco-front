5bec8a6d6b4ea2442184fd68ffea12e1103f424d
gabriel-victor933 <gabrielvictor933@usp.br>
2023-04-26 19:16:36 -0300

    adding reset css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App">Hello world! </div>;
+  return( 
+    <div>
+      Hello world!
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ResetStyle from './style/ResetStyle';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React>
+  <React.StrictMode>
+    <ResetStyle />
     <App />
-  </React>
+  </React.StrictMode>
 );
 
 
